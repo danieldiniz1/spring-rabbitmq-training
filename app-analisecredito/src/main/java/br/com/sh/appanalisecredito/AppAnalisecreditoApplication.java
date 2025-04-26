ec42018fd68a504ebdf0e4261ab19c7ec4a0c375
danieldiniz1 <danieldiniz@gmail.com>
2025-04-26 13:59:32 -0300

    create new packeage for analise credito

---
+++ app-analisecredito/src/main/java/br/com/sh/appanalisecredito/AppAnalisecreditoApplication.java
@@ -0,0 +1,13 @@
+package br.com.sh.appanalisecredito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAnalisecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAnalisecreditoApplication.class, args);
+	}
+
+}
---
+++ app-analisecredito/src/test/java/br/com/sh/appanalisecredito/AppAnalisecreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sh.appanalisecredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAnalisecreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
