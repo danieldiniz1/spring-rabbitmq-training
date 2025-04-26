8477eaeea766f388914c388ae24113368ff0f03f
danieldiniz1 <danieldiniz@gmail.com>
2025-04-26 19:36:49 -0300

    ms proposta consumindo fila de proposta com analise concluida

---
+++ app-proposta/src/main/java/br/com/sh/appproposta/exception/PropostaNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.sh.appproposta.exception;
+
+public class PropostaNotFoundException extends RuntimeException {
+
+    public PropostaNotFoundException(String s) {
+        super(s);
+    }
+}
